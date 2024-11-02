81fce8fb2da38e294cb4378dfa568842a95c050a
chen-chenglong <chenglongchen@qq.com>
2024-11-03 07:31:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '陈成龙', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '陈成龙的博客', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://chenchenglong.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '陈成龙,陈成龙的博客,chenchenglong,chenchenglong.com', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '陈成龙,陈成龙的博客,chenchenglong', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'chenglongchen@qq.com', // 邮箱地址 例如mail@tangly1024.com
