13ea15d5895e1792b7b46b8e35cf555a7155d6ed
chen-chenglong <chenglongchen@qq.com>
2024-11-30 10:12:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,10 +24,10 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '积木好课', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '积木好课官方网站', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://jimuhaoke.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '积木好课,jimuhaoke,积木好课课程', // 网站关键词 英文逗号隔开
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '木仓学院', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '木仓学院官方网站', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://mucangxy.com', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '木仓学院', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'chenglongchen@qq.com', // 邮箱地址 例如mail@tangly1024.com
