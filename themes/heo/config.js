242d8331fadde32dfc03eb744d5859089c48c792
chen-chenglong <chenglongchen@qq.com>
2025-04-02 21:34:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://chenchenglong.com' },
+    { title: '访问学习手册快速开始学习', url: 'https://chenchenglong.com/article/1' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '服务支持', url: '/tag/服务支持' },
+  HEO_HERO_CATEGORY_2: { title: 'AutoCAD', url: '/tag/AutoCAD' },
+  HEO_HERO_CATEGORY_3: { title: 'Solidworks', url: '/tag/solidworks' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -40,12 +40,12 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '分享与热心帮助',
+    '专修交互与设计',
+    '脚踏实地行动派',
+    '智能家居小能手',
+    '数码科技爱好者',
+    '团队小组发动机'
   ],
 
   // 个人资料底部按钮
@@ -123,10 +123,10 @@ const CONFIG = {
   HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // 底部统计面板文案
-  HEO_POST_COUNT_TITLE: '文章数:',
+  HEO_POST_COUNT_TITLE: '文章计数:',
   HEO_SITE_TIME_TITLE: '建站天数:',
-  HEO_SITE_VISIT_TITLE: '访问量:',
-  HEO_SITE_VISITOR_TITLE: '访客数:',
+  HEO_SITE_VISIT_TITLE: '访问总量:',
+  HEO_SITE_VISITOR_TITLE: '访客计数:',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
