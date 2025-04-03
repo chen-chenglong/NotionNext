01d21f36391db36c377bac75ec8c23e3f4a028ef
chen-chenglong <chenglongchen@qq.com>
2025-04-03 10:26:48 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -30,7 +30,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '服务支持', url: '/category/服务支持' },
   HEO_HERO_CATEGORY_2: { title: 'AutoCAD', url: '/category/AutoCAD' },
-  HEO_HERO_CATEGORY_3: { title: 'Solidworks', url: '/category/solidworks' },
+  HEO_HERO_CATEGORY_3: { title: 'Solidworks', url: '/category/Solidworks' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
