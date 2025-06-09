cb2769d78dffcef1448b5a3e3a9816ca38a43fc5
chen-chenglong <chenglongchen@qq.com>
2025-06-09 19:30:50 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: 'AutoCAD', url: '/tag/AutoCAD' },
-  HEO_HERO_CATEGORY_2: { title: '科研绘图', url: '/tag/科研绘图' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'AutoCAD', url: '/category/AutoCAD' },
+  HEO_HERO_CATEGORY_2: { title: '科研绘图', url: '/category/科研绘图' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/category/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
