b9246dc47c12febad3aa09e44acc0e285f9e9e21
chen-chenglong <chenglongchen@qq.com>
2025-08-31 07:39:27 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -29,8 +29,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: 'AutoCAD', url: '/category/AutoCAD' },
-  HEO_HERO_CATEGORY_2: { title: 'CAD案例', url: '/category/CAD案例' },
-  HEO_HERO_CATEGORY_3: { title: '技术服务', url: '/category/技术服务' },
+  HEO_HERO_CATEGORY_2: { title: '1对1教学', url: '/category/一对一教学' },
+  HEO_HERO_CATEGORY_3: { title: '便民服务', url: '/category/便民服务' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
